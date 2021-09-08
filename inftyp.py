50860bee9550d44f5d17e6764fcefb388ae3064d
Jonathan <jonathan97clausen@gmail.com>
2021-09-08 14:30:44 +0200

    Circle attempt 1

--- inftyp.py
+++ inftyp.py
@@ -1,13 +1,20 @@
 from time import sleep
-
+import math
 import ARLO.robot
 
 
-arlo = robot.Robot()
+arlo = ARLO.robot.Robot()
 
 
-def TurnRight():
-    print(arlo.go_diff(64, 32, 1, 1))
+def Circle(r,outerSpeed):
+    inner = 2 * (r - 19.4) * math.PI
+    outer = 2 * (r + 19.4) * math.PI
+
+    outerTime = outer / 0.622 * outerSpeed
+
+    innerSpeed = inner / 0.622 * outerTime
+
+    print(arlo.go_diff(outerSpeed, math.floor(innerSpeed * 0.97), 1, 1))
 
 
-while True:
+Circle(50,100)
