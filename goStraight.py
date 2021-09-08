72f3155f73e42fba129525b80b6eb457dd6bcade
Jonathan <jonathan97clausen@gmail.com>
2021-09-08 13:45:06 +0200

    added square

--- goStraight.py
+++ goStraight.py
@@ -12,10 +12,17 @@ print("Running ...")
 leftSpeed = math.floor(100 * 0.97)
 rightSpeed = 100
 
+for x in range(4):
 
 # Go straight
-print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
-sleep(0.803)
+    print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+    sleep(0.803)
+
+# turn
+    print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
+    sleep(0.5836)
+
+
 
 print(arlo.stop())
 
