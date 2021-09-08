d2fcfa8c729cc663f7ffabb140c959ddfed39319
Jonathan <jonathan97clausen@gmail.com>
2021-09-08 11:47:40 +0200

    straight test

---
+++ goStraight.py
@@ -0,0 +1,20 @@
+from time import sleep
+
+import ARLO.robot
+
+# Create a robot object and initialize
+arlo = ARLO.robot.Robot()
+
+print("Running ...")
+
+# send a go_diff command to drive forward
+leftSpeed = 64
+rightSpeed = 64
+
+
+# Go straight
+print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+sleep(3)
+
+print(arlo.stop())
+
