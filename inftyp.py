753cb1f42ea6dcc183159ecfdcf3a401cd0569fd
Jonathan <jonathan97clausen@gmail.com>
2021-09-13 13:18:09 +0200

    Added brackets

--- inftyp.py
+++ inftyp.py
@@ -10,9 +10,9 @@ def Circle(r,outerSpeed):
     inner = 2 * (r - 19.4) * math.PI
     outer = 2 * (r + 19.4) * math.PI
 
-    outerTime = outer / 0.622 * outerSpeed
+    outerTime = outer / (0.622 * outerSpeed)
 
-    innerSpeed = inner / 0.622 * outerTime
+    innerSpeed = inner / (0.622 * outerTime)
 
     print(arlo.go_diff(math.floor(outerSpeed * 0.97), innerSpeed, 1, 1))
 
