d63ab8230b210d3db8f4021c531b25d05f609c58
emil565a <emil.hansen1998@gmail.com>
2021-09-15 10:21:06 +0200

    Created obstacleAvoidance.py

---
+++ obstacleAvoid.py
@@ -0,0 +1,13 @@
+from time import sleep
+import math
+
+import ARLO.robot
+
+# Create a robot object and initialize
+arlo = ARLO.robot.Robot()
+
+print("Running ...")
+
+# send a go_diff command to drive forward
+leftSpeed = math.floor(64 * 0.97)
+rightSpeed = 64
