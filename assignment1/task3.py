5370a2aade0808b23e81b7d0c30b831a608f65e1
Carl Warming <carl.warming@gmail.com>
2021-09-16 16:44:25 +0200

    Assignment 1.3 complete Co-authored-by: emil565a <emil565a@users.noreply.github.com>

--- assignment1/task3.py
+++ assignment1/task3.py
@@ -6,8 +6,7 @@ import random as rnd
 import math
 
 def n(x, mu, sig):
-    n = 1/math.sqrt(2*math.pi)*sig*math.exp((-(1/2))*((x-mu)**2/sig**2))
-    return n
+    return 1/math.sqrt(2*math.pi)*sig*math.exp((-(1/2))*((x-mu)**2/sig**2))
 
 def p(x):
     return 0.3*n(x, 2, 1)+0.4*n(x, 5, 2)+0.3*n(x, 9, 1)
@@ -15,30 +14,79 @@ def p(x):
 def q(x):
     return 1/16
 
-    # 0.3*np.random.normal(2,1)+0.4*np.random.normal(5,2)+0.3*np.random.normal(9,1)
-    # return x
+def cum_Normalize(w, k):
+    res = np.zeros(k)
+    wSum = sum(w)
 
-def sampler(Size):
-    uni = np.random.uniform(low=0.0, high=15.0, size=Size)
-    pVal   = np.zero(Size)
-    w   = np.zero(Size)
-    res = np.zero(Size)
+    # calculate normalized weights and acummulate them
+    res[0] = w[0]/wSum
+    for i in range(1,k):
+        res[i] = w[i]/wSum + res[i-1]
+    return res
 
-    for i in range(Size):
-        pVal[i] = p(uni[i])
-        w[i] = pVal[i]/q(i)
+def sampler(k, proposal):
+    w   = np.zeros(k)
+    res = np.zeros(k)
+    
+    #Importance
+    for i in range(k):
+        w[i] = p(proposal[i])/q(i)  
+    w_norm = cum_Normalize(w, k)
 
+    #Resampling
+    for i in range(k):
+        rand = np.random.sample(1)
+        for j in range(k):
+            if (rand <= w_norm[j]):
+                res[i] = proposal[j]
+                break
+    res.sort()
+    return res
 
-        # her efter var jeg træt tag intet for givet
-    sum = 0
-    for i in range(Size):
-        for j in range(Size):
-            rand = rnd(0.0, 1.0)
-            sum = sum + w[j]
-            if (sum > rand):
-                res[j] = res[j]+1
-
-
-
-    plt.plot(uni, )
+##################Plotting Spørgsmål 1#######################
+def make_Uni_plot(k):
+    #Selecting positions from the uniform distribution
+    proposal = np.random.uniform(low=0.0, high=15.0, size = k)
+    res = sampler(k, proposal)
+    #Plotting
+    plt.figure(1)
+    ax1 = plt.subplot(211)
+    plt.hist(proposal, label='Uniform distribution', color='red')
+    plt.hist(res, label='Nye samples', color='blue')
+    plt.legend()
+    plt.subplot(212, sharex=ax1)
+    y = np.zeros(100) 
+    x = np.linspace(0,15,100)
+    for i in range(100):
+        y[i] = p(x[i]) 
+    plt.plot(x,y, label='Robottens potentielle position')
+    plt.legend()
     plt.show()
+
+make_Uni_plot(20)
+make_Uni_plot(100)
+make_Uni_plot(1000)
+
+##################Plotting Spørgsmål 2#######################
+def make_norm_plot(k):
+    #Selecting positions from normal distribution
+    proposal = np.random.normal(5, 4, size = k)
+    res = sampler(k, proposal)
+
+    plt.figure(1)
+    ax1 = plt.subplot(211)
+    plt.hist(proposal, label='Normal distribution', color='red')
+    plt.hist(res, label='Nye samples', color='blue')
+    plt.legend()
+    plt.subplot(212, sharex=ax1)
+    y = np.zeros(100) 
+    x = np.linspace(-10,20,100)
+    for i in range(100):
+        y[i] = n(x[i], 5, 4) 
+    plt.plot(x,y, label='Robottens potentielle position')
+    plt.legend()
+    plt.show()
+
+make_norm_plot(20)
+make_norm_plot(100)
+make_norm_plot(1000)
