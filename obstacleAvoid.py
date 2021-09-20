d438374ad69b1e290dd1daf2bdce0837326f9ba5
emil565a <emil.hansen1998@gmail.com>
2021-09-20 15:07:32 +0200

    Updated

--- obstacleAvoid.py
+++ obstacleAvoid.py
@@ -79,7 +79,10 @@ def obstacle():
     arlo.stop()
                        
 while(True):
-    arlo.go_diff(leftSpeed, rightSpeed, 1, 1)
+    #arlo.go_diff(leftSpeed, rightSpeed, 1, 1)
+    dists_safe()
+    update_dists()
+    print("LF",left45Clear, "FC", frontClear, "RC", right45Clear)
     sleep(0.1)
     print("L", arlo.read_left_ping_sensor(), "F", arlo.read_front_ping_sensor(), "C", arlo.read_right_ping_sensor())
     if arlo.read_front_ping_sensor() < 500 or arlo.read_right_ping_sensor() < 200 or arlo.read_left_ping_sensor() < 200:
