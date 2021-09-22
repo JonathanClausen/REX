dbce2c184135d8fffce0471b8affca1ebbf6bc81
Jonathan <jonathan97clausen@gmail.com>
2021-09-22 15:13:22 +0200

    test

--- testturn.py
+++ testturn.py
@@ -8,7 +8,7 @@ arlo = ARLO.robot.Robot()
 leftSpeed     = math.floor(64 * 0.97)
 rightSpeed    = 64
 degSec90 = 0.005
-degSec10 = 0.005
+degSec10 = 0.009
 
 
 print(arlo.go_diff(leftSpeed, rightSpeed, 0, not 0))
