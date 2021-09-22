51b84ab165c29d31c1d2149af5ad92fab3d46a1c
Jonathan <jonathan97clausen@gmail.com>
2021-09-22 14:44:00 +0200

    arlo

--- testturn.py
+++ testturn.py
@@ -3,10 +3,12 @@ import numpy as np
 from time import sleep
 import ARLO.robot
 
+arlo = ARLO.robot.Robot()
+
 leftSpeed     = math.floor(64 * 0.97)
 rightSpeed    = 64
 degSec = 0.005
 
-print(ARLO.go_diff(leftSpeed, rightSpeed, 0, not 0))
+print(arlo.go_diff(leftSpeed, rightSpeed, 0, not 0))
 sleep(45 * degSec)
-print(ARLO.stop())
+print(arlo.stop())
