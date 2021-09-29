42b87defea16264e52398060c0087f0c7fee6879
Jonathan <jonathan97clausen@gmail.com>
2021-09-29 16:13:10 +0200

    added

---
+++ Ex4-self-localization/src/handout/python/findlocation.py
@@ -0,0 +1,23 @@
+import sys
+import math
+from time import sleep
+
+
+# Get ARLO.Robot
+sys.path.append("../../../../ARLO/")
+import ARLO.robot as arlo
+
+def localization_turn():
+    leftSpeed = math.floor(64 * 0.97)
+    rightSpeed = 64
+    degSec10 = 0.005
+    timesToTurn = 16
+
+    for i in range(timesToTurn):
+        print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
+        sleep(2 * degSec10)
+        print(arlo.stop())
+
+        ## Check surroundings for box
+        ## Update samples to turn 20 degrees.
+
