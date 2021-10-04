a549a866f2782fb769fe9440521c420df6842ed7
Jonathan <jonathan97clausen@gmail.com>
2021-10-04 14:39:35 +0200

    19 times

--- Ex4-self-localization/findlocation.py
+++ Ex4-self-localization/findlocation.py
@@ -15,7 +15,7 @@ def localization_turn():
     rightSpeed = 64
     degSec = 0.005
     deg = 30
-    timesToTurn = 21
+    timesToTurn = 19
 
     for i in range(timesToTurn):
         print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
