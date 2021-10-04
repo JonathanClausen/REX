b00e346d00a446a67b1880917b4477e6904ec900
AGS-Denmark <antongehl@hotmail.com>
2021-10-04 16:22:08 +0200

    localyzer v2 init

---
+++ Ex4-self-localization/localizeV2.py
@@ -0,0 +1,247 @@
+#!/usr/bin/env python3
+
+from numpy.linalg.linalg import norm
+import cv2
+import particle
+import camera
+import numpy as np
+import time
+import math
+import random
+from timeit import default_timer as timer
+import sys
+import copy
+from time import sleep
+
+sys.path.append("../ARLO/")
+
+
+# a is a uniform number between 0 and 1
+def chooseSample(Weights, a):
+    i = 0
+    j = len(Weights)-1
+
+    while (True):
+        b = math.ceil(((j - i) / 2) + i)
+        if (a == Weights[b]):
+            return b
+        elif (a > Weights[b]):
+            i = b
+        else:
+            if j == b:
+                return b
+            else:
+                j = b
+
+
+#Simple Randomizer
+def randomizer(particles, p):
+    n = len(particles)
+    nRandom = math.floor((len(particles)/100*p))
+    for p in range(nRandom):
+        particles[p] = particle.Particle(600.0*np.random.ranf() - 100.0, 600.0*np.random.ranf() - 250.0,
+                                             np.mod(2.0*np.pi*np.random.ranf(), 2.0*np.pi), 0.01/n)
+
+
+# Some color constants in BGR format
+CRED = (0, 0, 255)
+CGREEN = (0, 255, 0)
+CBLUE = (255, 0, 0)
+CCYAN = (255, 255, 0)
+CYELLOW = (0, 255, 255)
+CMAGENTA = (255, 0, 255)
+CWHITE = (255, 255, 255)
+CBLACK = (0, 0, 0)
+
+# Landmarks.
+# The robot knows the position of 2 landmarks. Their coordinates are in the unit centimeters [cm].
+landmarkIDs = [1, 3 ]
+landmarks = {
+
+    1: (0.0, 0.0),  # Coordinates for landmark 1
+    3: (300.0, 0.0)   # Coordinates for landmark 2
+    #4: (300.0, 0.0)  # Coordinates for landmark 2
+}
+landmark_colors = [CRED, CGREEN] # Colors used when drawing the landmarks
+
+
+
+
+
+def jet(x):
+    """Colour map for drawing particles. This function determines the colour of
+    a particle from its weight."""
+    r = (x >= 3.0/8.0 and x < 5.0/8.0) * (4.0 * x - 3.0/2.0) + (x >= 5.0/8.0 and x < 7.0/8.0) + (x >= 7.0/8.0) * (-4.0 * x + 9.0/2.0)
+    g = (x >= 1.0/8.0 and x < 3.0/8.0) * (4.0 * x - 1.0/2.0) + (x >= 3.0/8.0 and x < 5.0/8.0) + (x >= 5.0/8.0 and x < 7.0/8.0) * (-4.0 * x + 7.0/2.0)
+    b = (x < 1.0/8.0) * (4.0 * x + 1.0/2.0) + (x >= 1.0/8.0 and x < 3.0/8.0) + (x >= 3.0/8.0 and x < 5.0/8.0) * (-4.0 * x + 5.0/2.0)
+
+    return (255.0*r, 255.0*g, 255.0*b)
+
+def draw_world(est_pose, particles, world):
+    """Visualization.
+    This functions draws robots position in the world coordinate system."""
+
+    # Fix the origin of the coordinate system
+    offsetX = 100
+    offsetY = 250
+
+    # Constant needed for transforming from world coordinates to screen coordinates (flip the y-axis)
+    ymax = world.shape[0]
+
+    world[:] = CWHITE # Clear background to white
+
+    # Find largest weight
+    max_weight = 0
+    for particle in particles:
+        max_weight = max(max_weight, particle.getWeight())
+
+    # Draw particles
+    for particle in particles:
+        x = int(particle.getX() + offsetX)
+        y = ymax - (int(particle.getY() + offsetY))
+        colour = jet(particle.getWeight() / max_weight)
+        cv2.circle(world, (x,y), 2, colour, 2)
+        b = (int(particle.getX() + 15.0*np.cos(particle.getTheta()))+offsetX,
+                                     ymax - (int(particle.getY() + 15.0*np.sin(particle.getTheta()))+offsetY))
+        cv2.line(world, (x,y), b, colour, 2)
+
+    # Draw landmarks
+    for i in range(len(landmarkIDs)):
+        ID = landmarkIDs[i]
+        lm = (int(landmarks[ID][0] + offsetX), int(ymax - (landmarks[ID][1] + offsetY)))
+        cv2.circle(world, lm, 5, landmark_colors[i], 2)
+
+    # Draw estimated robot pose
+    a = (int(est_pose.getX())+offsetX, ymax-(int(est_pose.getY())+offsetY))
+    b = (int(est_pose.getX() + 15.0*np.cos(est_pose.getTheta()))+offsetX,
+                                 ymax-(int(est_pose.getY() + 15.0*np.sin(est_pose.getTheta()))+offsetY))
+    cv2.circle(world, a, 5, CMAGENTA, 2)
+    cv2.line(world, a, b, CMAGENTA, 2)
+
+
+
+def localize(numResample, particles, debug):
+    est_pose = particle.estimate_pose(particles) # The estimate of the robots current pose
+    num_particles = len(particles)
+
+
+    world = np.zeros((500,500,3), dtype=np.uint8)
+    if debug:
+        # Open windows
+        WIN_RF1 = "Robot view"
+        cv2.namedWindow(WIN_RF1)
+        cv2.moveWindow(WIN_RF1, 50, 50)
+
+        WIN_World = "World view"
+        cv2.namedWindow(WIN_World)
+        cv2.moveWindow(WIN_World, 500, 50)
+
+        # Allocate space for world map
+        draw_world(est_pose, particles, world)
+
+
+    cam = camera.Camera(0)
+    varNorm = 30
+    varTheta = 0.1
+    varPos = 3
+    varOri = 0.1
+    for run in range(numResample):
+        if debug:
+            print("Runnig sampling ", run)
+
+        # Fetch next frame
+        colour = cam.get_next_frame()
+        # Detect objects
+        objectIDs, dists, angles = cam.detect_aruco_objects(colour)
+        if not isinstance(objectIDs, type(None)):
+            # List detected objects
+            for i in range(len(objectIDs)):
+                #print("Object ID = ", objectIDs[i], ", Distance = ", dists[i], ", angle = ", angles[i])
+                # XXX: Do something for each detected object - remember, the same ID may appear several times
+                lx = (landmarks[objectIDs[i]])[0]
+                ly = (landmarks[objectIDs[i]])[1]
+                sumWeight = 0
+                for p in particles:
+                    # Calculating coordinate weights
+                    pDist = math.sqrt( ( lx - p.getX() )**2 + ( ly - p.getY() )**2 )
+                    pPosWeight =  1/math.sqrt(2*math.pi * varNorm**2) * math.exp(- ((dists[i] - pDist )**2) / (2 * varNorm**2))
+
+                    # Calculating orientation weight
+
+                    e0 = np.array([ [ math.cos(p.getTheta()) ],[ math.sin(p.getTheta())]])
+                    el = (np.array([ [ lx - p.getX() ],[ ly - p.getY() ] ])) / pDist
+                    eHat = np.array([[ - math.sin( p.getTheta() )], [ math.cos( p.getTheta() )]])
+
+                    pPhi = (np.sign(el.T @ eHat)) * (np.arccos(el.T @ e0))
+
+                    pOrientWeight = 1/math.sqrt(2*math.pi * varTheta**2) * math.exp(- ((angles[i] - pPhi )**2) / (2 * varTheta**2))
+
+                    pWeight = pPosWeight * pOrientWeight
+
+                    sumWeight += pWeight
+                    p.setWeight(pWeight)
+
+
+                # Normalize weights
+                newParticles = copy.deepcopy(particles)
+                cumNormWeights = np.zeros(len(particles))
+
+                #normWeight = np.zeros(num_particles)
+                for p in range(len(particles)):
+                    normWeight = (particles[p].getWeight()/sumWeight)
+                    newParticles[p].setWeight(normWeight)
+                    if p == 0:
+                        cumNormWeights[p] = normWeight
+                    else:
+                        cumNormWeights[p] = normWeight + cumNormWeights[p-1]
+
+
+                #Resampling
+                for p in range(len(particles)):
+                    a = random.uniform(0.0, 1.0)
+                    i = chooseSample(cumNormWeights, a)
+                    newParticles[p] = copy.copy(particles[i])
+                particle.add_uncertainty(newParticles, varPos, varOri)
+                particles = copy.deepcopy(newParticles)
+                #randomizer(particles,0.5)
+
+            est_pose = particle.estimate_pose(particles) # The estimate of the robots current pose
+
+            if debug:
+                print("est_pose X = ", est_pose.getX(), " Y = ", est_pose.getY(), " theta = ", est_pose.getTheta())
+                # Draw map
+                draw_world(est_pose, particles, world)
+                # Show frame
+                cv2.imshow(WIN_RF1, colour)
+                # Show world
+                cv2.imshow(WIN_World, world)
+
+    return est_pose
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def initialize_particles(num_particles):
+    particles = []
+    for i in range(num_particles):
+        # Random starting points.
+        p = particle.Particle(600.0*np.random.ranf() - 100.0, 600.0*np.random.ranf() - 250.0, np.mod(2.0*np.pi*np.random.ranf(), 2.0*np.pi), 1.0/num_particles)
+        particles.append(p)
+
+    return particles
+
+particles = initialize_particles(1000)
+pos = localize(5, particles, 1)
+print("X = ", pos.getX(), " - Y = ", pos.getY(), " - Theta = ", pos.getTheta())
