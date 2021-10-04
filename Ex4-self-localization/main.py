db98fa31b5236880b841b950cd4ede2faabb9f96
Jonathan <jonathan97clausen@gmail.com>
2021-10-04 17:06:11 +0200

    remove while

--- Ex4-self-localization/main.py
+++ Ex4-self-localization/main.py
@@ -15,22 +15,21 @@ particles = selflocalize.initialize_particles(num_particles)
 #initial_location = findlocation.localization_turn(particles)
 
 # Main loop
-finished = False
-while (not finished):
-
-    # Drive towards middle of boxes
 
 
-    # check location.
-    meanParticle = localize.localize(10, particles, 0)
-    vecLength, targetOri = findlocation.estimate_target(0,300, meanParticle)
-    distToTarget = math.sqrt(( target[0] - meanParticle.getX() )**2 + ( target[1] - meanParticle.getY() )**2)
-    
-    #Moving
-    #move.turnAll(targetOri, particles) 
-    move.moveAll(vecLength, particles)
-    if (distToTarget < 10):
-        finished = True
+# Drive towards middle of boxes
+
+
+# check location.
+meanParticle = localize.localize(10, particles, 0)
+vecLength, targetOri = findlocation.estimate_target(0,300, meanParticle)
+distToTarget = math.sqrt(( target[0] - meanParticle.getX() )**2 + ( target[1] - meanParticle.getY() )**2)
+
+#Moving
+#move.turnAll(targetOri, particles) 
+move.moveAll(vecLength, particles)
+if (distToTarget < 10):
+    finished = True
     
 
 
