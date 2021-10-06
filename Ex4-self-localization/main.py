6324ce6c85d926aa402445094499b441acf7b32a
Jonathan <jonathan97clausen@gmail.com>
2021-10-06 15:23:39 +0200

    changes

--- Ex4-self-localization/main.py
+++ Ex4-self-localization/main.py
@@ -15,35 +15,36 @@ import ARLO.robot
 
 try:
     arlo = ARLO.robot.Robot()
-    cam = camera.Camera(0, 'arlo', useCaptureThread = True)
+    cam = camera.Camera(0, 'arlo', useCaptureThread = True, matrix)
 
     target = np.array([150,0])
     # Initialize particles
     num_particles = 1000
+
+
     particles = localize.initialize_particles(num_particles)
-
+    distToTarget = 100
     # Turn 360 until we find boxes (findlocation.py)
+    while (distToTarget > 5):
+        particles = copy.deepcopy(findlocation.localization_turn(particles, arlo, cam))
+        
+        # check location. Keep spinning
+        print("meanParticle = ")
+        meanParticle = particle.estimate_pose(particles)
+        print(meanParticle.getX(), ", ", meanParticle.getY(), ", ", meanParticle.getTheta())
 
-    particles = copy.deepcopy(findlocation.localization_turn(particles, arlo, cam))
+        #print("estimatetarget = ")
+        vecLength, targetOri = findlocation.estimate_target(target[0], target[1], meanParticle)
+        #print(vecLength)
+
+        print("disttotarget = ")
+        distToTarget = math.sqrt(( target[0] - meanParticle.getX() )**2 + ( target[1] - meanParticle.getY() )**2)
+        print(distToTarget)
 
 
-    # check location. Keep spinning
-    print("meanParticle = ")
-    meanParticle = particle.estimate_pose(particles)
-    print(meanParticle.getX(), ", ", meanParticle.getY(), ", ", meanParticle.getTheta())
-
-    #print("estimatetarget = ")
-    vecLength, targetOri = findlocation.estimate_target(target[0], target[1], meanParticle)
-    #print(vecLength)
-
-    print("disttotarget = ")
-    distToTarget = math.sqrt(( target[0] - meanParticle.getX() )**2 + ( target[1] - meanParticle.getY() )**2)
-    print(distToTarget)
-
-
-    move.turnAll((targetOri), particles, arlo) 
-    sleep(1)
-    move.moveAll(distToTarget, particles, arlo)
+        move.turnAll((targetOri), particles, arlo) 
+        sleep(1)
+        move.moveAll(distToTarget, particles, arlo)
     
 
 finally:
