6b5a20e9b09f58fa540df13056fc11e2ac094f09
Jonathan <jonathan97clausen@gmail.com>
2021-10-11 16:51:06 +0200

    Finally Works! THANK GOD
    Co-authored-by: Invixha <Invixha@users.noreply.github.com>

--- Ex5_Occupancy/Occupancy.py
+++ Ex5_Occupancy/Occupancy.py
@@ -6,23 +6,39 @@ import math
 #Import arlo robot
 sys.path.append("../")
 
-sys.path.append("../Ex4-self-localization")
 import ARLO.robot
-import particle
+import Ex4.particle
 
-def occupancy_grid_mapping(grid, ):
-    update_m = perceptualField(m,xt,zt)
+grid_size = 10
+sensor_max = 250
+sensor_angle = math.radians(15 * 2)
+
+
+## Log function
+prob_free = 0.9
+l_0 = prob_free
+l_occ = np.log((1-prob_free) / prob_free)
+l_free = np.log(prob_free / (1 - prob_free))
+
+def occupancy_grid_mapping(grid, mean_particle, sensors):
+    update_m = perceptualField(m,mean_particle,sensors)
     for m in update_m:
         x = m[0] 
         y = m[1]
-        grid[x][y] = grid[x][y] + inverse_range_sensor_model([x, y], mean_particle, sensation)
+        grid[x][y] = grid[x][y] + inverse_range_sensor_model([x, y, grid[x][y]], mean_particle, sensors) - l_0
             
 
 def inverse_range_sensor_model(cell,robot,sensor):
     r = math.sqrt(((cell[0] - robot.getX())**2) + ((cell[1] - robot.getY())**2) )
     angle = math.atan2( cell[1] - robot.getY, cell[0] - robot.getX() ) - robot.getTheta()
-
-
+    
+    if (r > (max(sensor_max, sensor + (grid_size**2 / 2))) or  abs(angle) > sensor_angle  ):
+        return cell[2]
+    if (sensor < sensor_max) and (abs(r - sensor) < grid_size/2):
+        return l_occ
+    if (r < sensor):
+        return l_free
+        
 def perceptualField(m, robot):
     
     
