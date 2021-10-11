6589f637c72b1b7105d72a7e5a47c1d358c41b37
emil565a <emil.hansen1998@gmail.com>
2021-10-11 14:55:25 +0200

    sensorTest

---
+++ Ex5_Occupancy/Occupancy.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def occupancy_grid_mapping(map, ):
+    for i in range(len(map)):
+        for j in range(len(map)):
+            
+
+def inverse_range_sensor_model(mi,xt,zt):
+    
+
+
+def perceptualField(m, xt, zt):
+    
+    
+
+     
---
+++ sensorTest.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from time import sleep
+import ARLO.robot as robot
+arlo = robot.Robot()
+# from threading import Thread, Lock
+
+
+sensFront     = 0
+
+sensInterval = 1
+
+sensFront = arlo.read_front_ping_sensor()
+print("Measurement: ", round(sensFront / 10 , 5) )
