9d7d178c8128932a9dbfaee303017b197de3b220
emil565a <emil.hansen1998@gmail.com>
2021-10-13 14:28:57 +0200

    added lokcation_turn2 function

--- Ex4/findlocation.py
+++ Ex4/findlocation.py
@@ -1,10 +1,15 @@
 import math
 from time import sleep
 import numpy as np
+from numpy.core.fromnumeric import put
+from ARLO.robot import Robot
+from Ex5_Occupancy.Occupancy import occupancy_grid_mapping
 import localize
 import move
 import camera
 import copy
+import particle 
+
 
 
 
@@ -46,7 +51,49 @@ def localization_turn(particles, arlo, cam):
 
         ## Update samples to turn 20 degrees.
     return particles
+
+
+def localization_turn2(particles, arlo, cam, map):
+    deg = 15
+    counter = 0
+    max_turn = 24
+    landmarks = []
+    seenBoth = False
+
+    #Variables for OccupancyMap
+
+    while((not seenBoth) and (counter <= max_turn)):
         
+        #Turn particles and update particles 
+        move.turnAll(math.radians(deg), particles, arlo)
+        #meanParticle = localize.localize(2, particles, 0, cam)
+        sleep(0.5)
+        # Check if both boxes have been spotted.
+        colour = cam.get_next_frame()
+        sleep(0.5)
+        objectIDs, dists, angles = cam.detect_aruco_objects(colour)
+
+        if not isinstance(objectIDs, type(None)):
+            particles = copy.deepcopy(localize.localize(1, particles, 0, cam))
+            for i in range(len(objectIDs)):
+                if (objectIDs[i] not in landmarks):
+                    landmarks.append(objectIDs[i])
+                    
+                    print("Found landmark: ", objectIDs[i])
+        if ((3 in landmarks) and (1 in landmarks)):
+            seenBoth = True
+
+        #Creating occupancy map 
+        distToObject = arlo.frontping()
+        newMap = occupancy_grid_mapping(map, particle.estimate_pose(particles), distToObject)
+        map = newMap
+
+        counter += 1
+
+        ## Update samples to turn 20 degrees.
+    return particles, map
+
+
 def estimate_target(targetX, targetY, p):
     vecX = targetX - p.getX()
     vecY = targetY - p.getY()
