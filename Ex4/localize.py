e1f837a637bfab43f20a4af2ddbe1dbec3b7b6ab
emil565a <emil.hansen1998@gmail.com>
2021-10-13 14:44:52 +0200

    Trying to pull

--- Ex4/localize.py
+++ Ex4/localize.py
@@ -54,7 +54,7 @@ CBLACK = (0, 0, 0)
 
 # Landmarks.
 # The robot knows the position of 2 landmarks. Their coordinates are in the unit centimeters [cm].
-landmarkIDs = [1, 3 ]
+landmarkIDs = [7, 1 ]
 landmarks = {
 
     1: (0.0, 0.0),  # Coordinates for landmark 1
