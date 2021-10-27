a437fdb17cf7ed5db4354a10c9eabb8a2eb61f3f
Jonathan <jonathan97clausen@gmail.com>
2021-10-27 12:56:32 +0200

    fix

--- Exam/gotobox.py
+++ Exam/gotobox.py
@@ -117,7 +117,7 @@ def go(targetBoxID, arlo, cam):
 
 def run_goToBox(targetBoxID, arlo, cam):
     while (getAng(targetBoxID, cam) == [0.0,0.0]):
-        turn(getAng(targetBoxID, cam)[0], arlo)
+        turn(10, arlo)
 
     turn(getAng(targetBoxID, cam)[0], arlo)
     return go(targetBoxID, arlo, cam)
