46593499292f62ec145d49cf20afa94ababc7dc2
Jonathan <jonathan97clausen@gmail.com>
2021-10-27 09:55:15 +0200

    gotobox

--- Exam/main.py
+++ Exam/main.py
@@ -8,6 +8,7 @@ import sys
 import camera
 from time import sleep
 import copy
+import gotobox
 
 #Import arlo robot
 sys.path.append("../")
@@ -25,22 +26,22 @@ try:
     perimiterToTargets = 40
     # Landmarks.
     # The robot knows the position of 2 landmarks. Their coordinates are in the unit centimeters [cm].
-    landmarkIDs = [1,9,3,4]
+    landmarkIDs = [1,2,3,4]
     landmarks = {
         1: (0.0, 0.0),  # Coordinates for landmark 1
-        9: (0.0, 300.0),
+        2: (0.0, 300.0),
         3: (400.0, 0.0),
         4: (400.0, 300.0)
     }
 
-    nextLandmark = 0
+    nextLandmarkIndex = 0
     
     particles = localize.initialize_particles(num_particles)
     distToTarget = 100
     # Initializing target
-    target = [landmarks[landmarkIDs[nextLandmark]][0], landmarks[landmarkIDs[nextLandmark]][1]]
+    target = [landmarks[landmarkIDs[nextLandmarkIndex]][0], landmarks[landmarkIDs[nextLandmarkIndex]][1]]
 
-    while (nextLandmark < 4):
+    while (nextLandmarkIndex < 4):
         
         particles = copy.deepcopy(findlocation.localization_turn(particles, arlo, landmarks, cam)) 
         
@@ -51,9 +52,9 @@ try:
 
         # Update target to next target in landmarks if current target reached.
         if (distToTarget < 30):
-            nextLandmark += 1
-            print("Next target is: ", landmarkIDs[nextLandmark])
-            target = [landmarks[landmarkIDs[nextLandmark]][0], landmarks[landmarkIDs[nextLandmark]][1]]
+            nextLandmarkIndex += 1
+            print("Next target is: ", landmarkIDs[nextLandmarkIndex])
+            target = [landmarks[landmarkIDs[nextLandmarkIndex]][0], landmarks[landmarkIDs[nextLandmarkIndex]][1]]
 
         # Adjusting target so we don't run into the box
         targetPerimiter = findlocation.adjusted_target(meanParticle, target, perimiterToTargets)
@@ -65,7 +66,8 @@ try:
 
         move.turnAll(targetOri, particles, arlo) 
         sleep(1)
-        move.moveAll(round(distToTarget * 1.10, 5), particles, arlo)
+        distTraveled = gotobox.run_goToBox(landmarkIDs[nextLandmarkIndex], arlo)
+        move.moveAllParticles(distTraveled, particles)
         
 
 finally:
