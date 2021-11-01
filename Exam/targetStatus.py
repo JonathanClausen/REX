77316ae2e74159db7e68cfeb39f1ae4a18e3c77e
emil565a <emil.hansen1998@gmail.com>
2021-11-01 15:29:12 +0100

    agagin

--- Exam/targetStatus.py
+++ Exam/targetStatus.py
@@ -3,8 +3,8 @@
 def checkTargetStatus(target, cam):
     frame = cam.get_next_frame()
     ids, dists, angles = cam.detect_aruco_objects(frame)
-    print(ids)
-    if ids is None: 
+    print("Checktarget: boxex in picture = " ,str(ids))
+    if isinstance(ids, type(None)): 
         return 2    
     elif target in ids: 
         return 0
