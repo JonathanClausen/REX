545c7cd307b974b22a657796bcc84acb81ab510e
emil565a <emil.hansen1998@gmail.com>
2021-11-01 14:19:53 +0100

    Sammenflettet og klar til kørsel 1.0
    Co-authored-by: Invixha <Invixha@users.noreply.github.com>
    Co-authored-by: JonathanClausen <JonathanClausen@users.noreply.github.com>

---
+++ Exam/targetStatus.py
@@ -0,0 +1,13 @@
+
+
+def checktargetStatus(target, cam):
+    cam.getnextframe()
+    frame = cam.get_next_frame()
+    ids, dists, angles = cam.detect_aruco_objects(frame)
+    if not(bool(ids)): 
+        return 2    
+    elif target in ids: 
+        return 0
+    else:
+        return 1
+  
