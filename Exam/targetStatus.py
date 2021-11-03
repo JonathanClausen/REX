c509c5624f735a7d98bc3378ce5c1c2dc4369413
emil565a <emil.hansen1998@gmail.com>
2021-11-03 16:31:15 +0100

    again

--- Exam/targetStatus.py
+++ Exam/targetStatus.py
@@ -9,19 +9,20 @@ def checkTargetStatus(target, cam):
     ids, dists, angles = cam.detect_aruco_objects(frame)
     print("Checktarget: boxes in picture = " ,str(ids))
     
-    targetAngle = angles[np.where(ids == target)]
-    targetDist = dists[np.where(ids == target)]
-    boxAngles   = angles[np.where(ids != target)]
-    boxDists    = dists[np.where(ids != target)]
-    spaces = []
-    for i in range(len(boxAngles)): 
-        space_between_box_target = math.sin(boxAngles[i])*boxDists[i]
-        spaces.append(space_between_box_target)
+    
     
     if isinstance(ids, type(None)): 
         return 2 #Nothing is seen at all    
 
     elif target in ids:
+        targetAngle = angles[np.where(ids == target)]
+        targetDist = dists[np.where(ids == target)]
+        boxAngles   = angles[np.where(ids != target)]
+        boxDists    = dists[np.where(ids != target)]
+        spaces = []
+    for i in range(len(boxAngles)): 
+        space_between_box_target = math.sin(boxAngles[i])*boxDists[i]
+        spaces.append(space_between_box_target)
         if len(ids) == 1:     
             return 0 #Target is the only thing seen
         else: 
