9f84adc3c511e5431478db24a80441f042bcafb5
emil565a <emil.hansen1998@gmail.com>
2021-11-08 11:08:31 +0100

    wow

--- Exam/routePlan.py
+++ Exam/routePlan.py
@@ -36,7 +36,7 @@ def findWay(cam,targetID):
     goAroundDist = dists[goAroundIndex]
     goAroundAng = angles[goAroundIndex]
     goAroundID = objectIDs[goAroundIndex]
-    print("avoid object : ", goAroundID)
+    print("center object : ", goAroundID)
 
     # is the object to pass a landmark that is not the goal, then fail
     if ((goAroundID in [1, 2, 3, 4]) and (goAroundID != targetID)):
@@ -77,7 +77,7 @@ def findWay(cam,targetID):
             distRight.append(space)   # find de to nærmeste kasser til højre og venstre
             roboToBoxRight.append(dists[i])
     print(roboToBoxRight ," to my right")
-    print(roboToBoxLeft ," to my right")
+    print(roboToBoxLeft ," to my left")
 
     minLeft  = min(distLeft , default=9999999) # default hvis listen er tom
     minRight = min(distRight, default=9999999)   # hvis der er frit til en af siderne vælger den denne
@@ -90,20 +90,23 @@ def findWay(cam,targetID):
             print("left is clear")
             turn, dist = go_to_xy(distEmpty, goAroundDist)
             return (turn+goAroundAng, dist)
-        index = distLeft.index(minLeft)
-        leftBoxID = bLeft[index]
+        
 
         #Go for centerbox
         if ((goAroundDist < min(roboToBoxLeft)) and (targetID != goAroundID)):
+            index = distLeft.index(minLeft)
+            leftBoxID = bLeft[index]
             turn, dist = go_to_xy(minLeft,goAroundDist)
             print("going left of box ", goAroundID, " and before ", leftBoxID)
             return (turn+goAroundAng, dist)
         #Go for nearest leftbox
         else:
             # find vinkelen til kassen du skal Køre op på siden af
-            avioldBoxAng = angles[np.where(objectIDs == leftBoxID)[0]]
-            turn, dist = go_to_xy(minLeft, dists[np.where(objectIDs == leftBoxID)[0]])
-            print("avoiding ",leftBoxID, " to get closer to ", goAroundID)
+            index = distRight.index(minRight)
+            rightBoxID = bRight[index]
+            avioldBoxAng = angles[np.where(objectIDs == rightBoxID)[0]]
+            turn, dist = go_to_xy(minRight, dists[np.where(objectIDs == rightBoxID)[0]])
+            print("avoiding ",rightBoxID, " to get closer to ", goAroundID)
             if (avioldBoxAng < turn):
                 return (turn-avioldBoxAng, dist)
             else:
@@ -115,132 +118,147 @@ def findWay(cam,targetID):
             print("right is clear")
             turn, dist = go_to_xy(distEmpty,goAroundDist)
             return (0-(turn-goAroundAng), dist)
-        index = distRight.index(minRight)
-        rightBoxID = bRight[index]
         
         #Go for centerbox
         if ((goAroundDist < min(roboToBoxRight)) and (targetID != goAroundID)):
+            index = distRight.index(minRight)
+            rightBoxID = bRight[index]
+    
             turn, dist = go_to_xy(minRight,goAroundDist)
             print("going right of box ", goAroundID, " and before ", rightBoxID)
             return (0-(turn-goAroundAng), dist)
         #Go for nearest rightbox 
         else:
-            avioldBoxAng = angles[np.where(objectIDs == rightBoxID)[0]]
-            turn, dist = go_to_xy(minRight, dists[np.where(objectIDs == rightBoxID)[0]])
-            print("avoiding ", rightBoxID, " to get closer to ", goAroundID)
-            if (avioldBoxAng < turn):
-                return (turn+avioldBoxAng, dist)
-            else:
-                return (avioldBoxAng-turn, dist)
-def findWayOLD(cam, targetID):
-    # tag billede
-    frame = cam.get_next_frame()
-    # identificer alle kasser
-    objectIDs, dists, angles = cam.detect_aruco_objects(frame)
-    if (objectIDs is None):
-        print("error in route planning, no objects found")
-        return (0,0)
-    bLeft = []
-    distLeft = []
-    bRight = []
-    distRight = []
-    roboToBoxLeft = []
-    roboToBoxRight = []
-
-    # find the center box, the box that is in the way
-    goAroundIndex = np.argmin(np.abs(angles)) #min(enumerate(angles), key=lambda x: abs(x[1]))
-    goAroundDist = dists[goAroundIndex]
-    goAroundAng = angles[goAroundIndex]
-    goAroundID = objectIDs[goAroundIndex]
-    print("avoid object : ", goAroundID)
-
-    if ((goAroundID in [1, 2, 3, 4]) and (goAroundID != targetID)):
-        print("Error in route planning, not trusting current theta")
-        return (0,0)
-    
-    # removing the center box from the lists
-    for i in np.where(objectIDs == goAroundID): 
-        objectIDs = np.delete(objectIDs, i) 
-        dists = np.delete(dists, i)
-        angles = np.delete(angles, i)
-
-    # sort the objects to what is left and right of the center box
-    for i in range(len(objectIDs)):
-        #print("Object ID = ", objectIDs[i], ", Distance = ", dists[i], ", angle = ", angles[i])
-
-        # devide into two lists on +- angle
-        # insert the distance between center box and the other obstacle (i) -> space
-        space = math.sqrt(goAroundDist**2 + dists[i]**2 - 2*goAroundDist*dists[i] * (math.cos(angles[i] - goAroundAng)))
-
-        #print("dist between boxes", space)
-        #print("angle Center = ", goAroundAng, " ob i = ", angles[i],
-        #" angle between in deg= ", math.degrees((angles[i] - goAroundAng)))
-        if space > 25000:
-            print("ignoring due to distance over 2.5 m")
-        elif angles[i] > 0:
-            print(objectIDs[i] ," to my left")
-            bLeft.append(objectIDs[i])
-            distLeft.append(space)
-            roboToBoxLeft.append(dists[i])
-        else:
-            print(objectIDs[i] ," to my right")
-            bRight.append(objectIDs[i])
-            distRight.append(space)   # find de to nærmeste kasser til højre og venstre
-            roboToBoxRight.append(dists[i])
-
-
-    minLeft  = min(distLeft , default=9999999) # default hvis listen er tom
-    minRight = min(distRight, default=9999999)   # hvis der er frit til en af siderne vælger den denne
-    # og køre en meter ved siden af forhindringen
-    distEmpty = 200   # chossing the side with the most space
-    if (minLeft >= minRight):
-        if ((minLeft == 9999999) and (targetID != goAroundID)):
-            # left is free finding direction next to obstacle
-            print("left is clear")
-            turn, dist = go_to_xy(distEmpty, goAroundDist)
-            return (turn+goAroundAng, dist)
-        index = distLeft.index(minLeft)
-        leftBoxID = bLeft[index]
-
-        #Go for centerbox
-        if ((goAroundDist < min(roboToBoxLeft)) and (targetID != goAroundID)):
-            turn, dist = go_to_xy(minLeft,goAroundDist)
-            print("going between box ", goAroundID, " and ", leftBoxID)
-            return (turn+goAroundAng, dist)
-        #Go for nearest leftbox
-        else:
-            # find vinkelen til kassen du skal Køre op på siden af
+            index = distLeft.index(minLeft)
+            leftBoxID = bLeft[index]
             avioldBoxAng = angles[np.where(objectIDs == leftBoxID)[0]]
             turn, dist = go_to_xy(minLeft, dists[np.where(objectIDs == leftBoxID)[0]])
-            print("avoiding ",leftBoxID, " to get closer to ", goAroundID)
-            if (avioldBoxAng < turn):
-                return (turn-avioldBoxAng, dist)
-            else:
-                return (0-(avioldBoxAng-turn), dist)
-
-    else:
-        if ((minRight == 9999999) and (targetID != goAroundID)):
-            # left is free finding direction next to obstacle
-            print("right is clear")
-            turn, dist = go_to_xy(distEmpty,goAroundDist)
-            return (0-(turn-goAroundAng), dist)
-        index = distRight.index(minRight)
-        rightBoxID = bRight[index]
-        
-        #Go for centerbox
-        if ((goAroundDist < min(roboToBoxRight)) and (targetID != goAroundID)):
-            turn, dist = go_to_xy(minRight,goAroundDist)
-            print("going between box ", goAroundID, " and ", rightBoxID)
-            return (0-(turn-goAroundAng), dist)
-        #Go for nearest rightbox 
-        else:
-            avioldBoxAng = angles[np.where(objectIDs == rightBoxID)[0]]
-            turn, dist = go_to_xy(minRight, dists[np.where(objectIDs == rightBoxID)[0]])
-            print("avoiding ", rightBoxID, " to get closer to ", goAroundID)
+            print("avoiding ", leftBoxID, " to get closer to ", goAroundID)
             if (avioldBoxAng < turn):
                 return (turn+avioldBoxAng, dist)
             else:
                 return (avioldBoxAng-turn, dist)
+
+
+
+
+
+
+
+
+
+
+
+
+# def findWayOLD(cam, targetID):
+#     # tag billede
+#     frame = cam.get_next_frame()
+#     # identificer alle kasser
+#     objectIDs, dists, angles = cam.detect_aruco_objects(frame)
+#     if (objectIDs is None):
+#         print("error in route planning, no objects found")
+#         return (0,0)
+#     bLeft = []
+#     distLeft = []
+#     bRight = []
+#     distRight = []
+#     roboToBoxLeft = []
+#     roboToBoxRight = []
+
+#     # find the center box, the box that is in the way
+#     goAroundIndex = np.argmin(np.abs(angles)) #min(enumerate(angles), key=lambda x: abs(x[1]))
+#     goAroundDist = dists[goAroundIndex]
+#     goAroundAng = angles[goAroundIndex]
+#     goAroundID = objectIDs[goAroundIndex]
+#     print("avoid object : ", goAroundID)
+
+#     if ((goAroundID in [1, 2, 3, 4]) and (goAroundID != targetID)):
+#         print("Error in route planning, not trusting current theta")
+#         return (0,0)
+    
+#     # removing the center box from the lists
+#     for i in np.where(objectIDs == goAroundID): 
+#         objectIDs = np.delete(objectIDs, i) 
+#         dists = np.delete(dists, i)
+#         angles = np.delete(angles, i)
+
+#     # sort the objects to what is left and right of the center box
+#     for i in range(len(objectIDs)):
+#         #print("Object ID = ", objectIDs[i], ", Distance = ", dists[i], ", angle = ", angles[i])
+
+#         # devide into two lists on +- angle
+#         # insert the distance between center box and the other obstacle (i) -> space
+#         space = math.sqrt(goAroundDist**2 + dists[i]**2 - 2*goAroundDist*dists[i] * (math.cos(angles[i] - goAroundAng)))
+
+#         #print("dist between boxes", space)
+#         #print("angle Center = ", goAroundAng, " ob i = ", angles[i],
+#         #" angle between in deg= ", math.degrees((angles[i] - goAroundAng)))
+#         if space > 25000:
+#             print("ignoring due to distance over 2.5 m")
+#         elif angles[i] > 0:
+#             print(objectIDs[i] ," to my left")
+#             bLeft.append(objectIDs[i])
+#             distLeft.append(space)
+#             roboToBoxLeft.append(dists[i])
+#         else:
+#             print(objectIDs[i] ," to my right")
+#             bRight.append(objectIDs[i])
+#             distRight.append(space)   # find de to nærmeste kasser til højre og venstre
+#             roboToBoxRight.append(dists[i])
+
+
+#     minLeft  = min(distLeft , default=9999999) # default hvis listen er tom
+#     minRight = min(distRight, default=9999999)   # hvis der er frit til en af siderne vælger den denne
+#     # og køre en meter ved siden af forhindringen
+#     distEmpty = 200   # chossing the side with the most space
+#     if (minLeft >= minRight):
+#         if ((minLeft == 9999999) and (targetID != goAroundID)):
+#             # left is free finding direction next to obstacle
+#             print("left is clear")
+#             turn, dist = go_to_xy(distEmpty, goAroundDist)
+#             return (turn+goAroundAng, dist)
+#         index = distLeft.index(minLeft)
+#         leftBoxID = bLeft[index]
+
+#         #Go for centerbox
+#         if ((goAroundDist < min(roboToBoxLeft)) and (targetID != goAroundID)):
+#             turn, dist = go_to_xy(minLeft,goAroundDist)
+#             print("going between box ", goAroundID, " and ", leftBoxID)
+#             return (turn+goAroundAng, dist)
+#         #Go for nearest leftbox
+#         else:
+#             # find vinkelen til kassen du skal Køre op på siden af
+#             avioldBoxAng = angles[np.where(objectIDs == leftBoxID)[0]]
+#             turn, dist = go_to_xy(minLeft, dists[np.where(objectIDs == leftBoxID)[0]])
+#             print("avoiding ",leftBoxID, " to get closer to ", goAroundID)
+#             if (avioldBoxAng < turn):
+#                 return (turn-avioldBoxAng, dist)
+#             else:
+#                 return (0-(avioldBoxAng-turn), dist)
+
+#     else:
+#         if ((minRight == 9999999) and (targetID != goAroundID)):
+#             # left is free finding direction next to obstacle
+#             print("right is clear")
+#             turn, dist = go_to_xy(distEmpty,goAroundDist)
+#             return (0-(turn-goAroundAng), dist)
+#         index = distRight.index(minRight)
+#         rightBoxID = bRight[index]
+        
+#         #Go for centerbox
+#         if ((goAroundDist < min(roboToBoxRight)) and (targetID != goAroundID)):
+#             turn, dist = go_to_xy(minRight,goAroundDist)
+#             print("going between box ", goAroundID, " and ", rightBoxID)
+#             return (0-(turn-goAroundAng), dist)
+#         #Go for nearest rightbox 
+#         else:
+#             avioldBoxAng = angles[np.where(objectIDs == rightBoxID)[0]]
+#             turn, dist = go_to_xy(minRight, dists[np.where(objectIDs == rightBoxID)[0]])
+#             print("avoiding ", rightBoxID, " to get closer to ", goAroundID)
+#             if (avioldBoxAng < turn):
+#                 return (turn+avioldBoxAng, dist)
+#             else:
+#                 return (avioldBoxAng-turn, dist)
 
 # for testing
 # try:
